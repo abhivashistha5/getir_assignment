5b0a83f30d24e64e4bfaf02ac23a16a0eb15e2e2
Abhijeet Vashistha <abhivashistha5@gmail.com>
2022-02-16 00:10:14 +0530

    establish connection to mongo db

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 import config from './config';
 import logger from './lib/logger';
 import healthRoute from './route/health';
@@ -7,10 +8,28 @@ import healthRoute from './route/health';
 const app = express();
 app.use(morgan(':date[iso] :method :url :status - :response-time ms'));
 
+// register routes
 app.use('/health', healthRoute);
 
-app.listen(config.http.port, () => {
-    logger.info(`Started listening on port: ${config.http.port}`);
-});
+// connect to database
+mongoose.connect(
+    config.database.connectionUri,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err) => {
+        if (err) {
+            logger.error(err);
+            process.exit(1);
+        }
+        logger.info('Mongo DB Connected');
+
+        // start the server
+        app.listen(config.http.port, () => {
+            logger.info(`Started listening on port: ${config.http.port}`);
+        });
+    },
+);
 
 export default app;
