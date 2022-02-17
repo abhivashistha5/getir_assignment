05d01c0b901be02edaa4b45d9918a42bb33f0db2
Abhijeet Vashistha <abhivashistha5@gmail.com>
2022-02-17 21:37:50 +0530

    enhanced date validation for record route

--- src/route/record/record.validation.js
+++ src/route/record/record.validation.js
@@ -1,11 +1,14 @@
-import Joi from 'joi';
+import baseJoi from 'joi';
+import dateExtention from '@joi/date';
 import config from '../../config';
+
+const Joi = baseJoi.extend(dateExtention);
 
 // eslint-disable-next-line import/prefer-default-export
 export const getRecordListValidation = Joi.object().keys(
     {
-        startDate: Joi.date().required(),
-        endDate: Joi.date().required(),
+        startDate: Joi.date().format('YYYY-MM-DD').required(),
+        endDate: Joi.date().format('YYYY-MM-DD').required(),
         minCount: Joi.number().min(0).required(),
         maxCount: Joi.number().min(0).required(),
     },
